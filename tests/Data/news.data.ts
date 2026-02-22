d2f1f0180097131691f5f15cf1fea8035a25bce3
Vitalii Pantak <Chillstepworld1@gmail.com>
2026-02-22 17:44:05 +0200

    Test CI

--- tests/Data/news.data.ts
+++ tests/Data/news.data.ts
@@ -2,9 +2,9 @@ import { Tags } from "@/enums/Tags";
 import { BASE_IMAGE_1, BASE_IMAGE_3 } from "./images.data";
 
 export const NEWS_CREATION_DATA = {
-    title: "WOOOOOOOOOF",
-    content: "WOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOF",
-    source: "https://www.woofcity.com.ua",
+    title: "Hello World",
+    content: "Hello World".repeat(5),
+    source: "https://www.helloworld.com.ua",
     image: BASE_IMAGE_1,
     tags: [Tags.ADS, Tags.EDUCATION, Tags.NEWS]
 }
@@ -12,9 +12,9 @@ export const NEWS_CREATION_DATA = {
 
 
 export const NEWS_CREATION_NONVALID_DATA = {
-    title: "WOOOOOOOOOF".repeat(30),
-    content: "WOOOF",
-    source: "WOOF",
+    title: "Hello World".repeat(30),
+    content: "Hello",
+    source: "Hello",
     image: BASE_IMAGE_3,
     tags: [Tags.INITIATIVES, Tags.EVENTS, Tags.NEWS]
 }
